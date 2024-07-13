08ec6529af94d1e9da3f535d63d050746d9f0b32
Diana <dianalop1998@gmail.com>
2024-07-12 23:17:28 -0400

    remove react logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
