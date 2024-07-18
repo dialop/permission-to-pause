54c828a7945b2f818a53fdc2a131f44d40397db2
Diana <dianalop1998@gmail.com>
2024-07-17 21:44:14 -0400

    Remove div information

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-       
+        
 
     </div>
   );
